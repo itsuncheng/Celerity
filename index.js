7349809a0b1b4764f6b2692f841b2e6005a6bbeb
Raymond <itsuncheng2000@gmail.com>
2018-07-29 05:38:14 +0800

    For heroku deployment

--- index.js
+++ index.js
@@ -13,13 +13,6 @@ app.get('/projects', function(req, res){
    res.sendFile(path.join(__dirname + '/public/projects.html'));
 });
 
-app.get('/research', function(req, res){
-   res.sendFile(path.join(__dirname + '/public/research.html'));
-});
 
-app.get('/contact', function(req, res){
-   res.sendFile(path.join(__dirname + '/public/contact.html'));
-});
-
-app.listen(3000)
-//app.listen(process.env.PORT || 5000)
+//app.listen(3000)
+app.listen(process.env.PORT || 5000)
